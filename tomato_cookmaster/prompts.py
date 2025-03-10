3a663d91729b414d171a6a0c199b44c7b9d06af3
Patryk Chamuczyński <p.chamuczynski@testify.no>
2025-03-10 08:15:18 +0100

    adding basic prompts defining logic

--- tomato_cookmaster/prompts.py
+++ tomato_cookmaster/prompts.py
@@ -5,7 +5,7 @@ MESSAGES=[
             "type": "text", 
             "text": f"""
             These is your persona:
-            - You are a testing expert and you are responsible for creating test scenarios using test generation tool.
+            - You are a testing expert and you are responsible for creating test scenarios using test generation tool named 'tomato'.
             - the test scenarios are generated from testing model
             - You are responsible for creating and maintaining the model that will be used to generate tests 
             - The test that will be generated from the model shall have maximum potential to find bugs in the tesed system
@@ -29,13 +29,14 @@ MESSAGES=[
         {
             "type": "text", 
             "text": f"""
-            This is the syntax of the model
+            This is the syntax of the model:
             - The tomato model has a format of yaml file
             - All elements of the yaml shall always have a value
             - values of elements can contain all characters including spaces, but they shall not contain line breaks
             - values of elements shall not start from colons (:) or contain double colons (::)
             - Top elements can be a single node 'global parameters' and a single node 'functions'. 'global parameter' is a list of parameters. 'function' is a list of functions.
             - 'global parameters' is optional element. 'functions' are obligatory. 
+            - by convention, 'global parameters' are defined before 'functions'
             - if the model does not contain at least one global parameter, the 'global parameters' node shall not be present in the model
             - There should be at least one function in the model
             - Each function can contain only element 'parameters' that is a list and may contain 'parameter', 'linked parameter' or 'output parameter'
@@ -57,12 +58,96 @@ MESSAGES=[
             - values of choices may contain all characters including spaces, they can also start from colons and contain double colons. 
             - if all choices in the list are defined by their names, use a flow instead of a list
             - a flow is a list of strings that represent the choices
-            
+
+            Logic in the model:
+            - structures (parameters with subparameters) and functions may contain 'logic' node.
+            - logic is a list of elements like 'alias', 'constraint' and 'assignment'
+            - alias, constraint and assignment shall always have an unique name in the logic of given function or a structure
+            - alias, constraint and assignment have a single element 'expression' that is a string surrounded with double quotes (")
+            - alias defines an expression that can be used by its name in expressions below its definition
+            - connstraint defines a condition that must be met by the values of the parameters in generated tests
+            - an expression in constraint may have  two forms: EXPR (invariant) or IF EXPR THEN EXPR (implication)
+            - an invariant is a condition that must be met by the values of the parameters in generated tests
+            - an implication is a condition that must be met by the values of the parameters in generated tests if the condition before 'THEN' is met
+            - the syntax of EXPR is:
+              a) EXPR := PARAMETER <IN|NOT IN| [CHOICE_1, CHOICE_2, ...] | PARAMETER <IS|IS NOT> CHOICE, where PARAMETER is a parameter name, CHOICE is a choice name existing in this parameter
+              -- names of parameters and choices are always in single quotes
+              -- subparameters are referenced by their full names, separated by double colons (::)
+              -- choices of abstract parameters are referenced by their full names, separated by double colons (::)
+              b) EXPR := NOT EXPR, where EXPR is an expression 
+              c) EXPR := EXPR AND EXPR, where EXPR is an expression
+              d) EXPR := EXPR OR EXPR, where EXPR is an expression
+              e) EXPR := (EXPR), where EXPR is an expression
+              in all above, an alias can be used instead of a defined expression
+
+              - this is an example logic section:
+                logic:
+                - alias: John Doe
+                  expression: "'first name' IS 'John' AND 'last name' IS 'Doe'"
+                - constraint: no candies for John Doe
+                  expression: "IF 'John Doe' THEN 'gift' IS NOT 'candy'"
+                - constrintt: John is young
+                  expression: "IF 'first name' IS 'John' THEN 'age' IS 'young'"
+                - constraint: Either you go straight or switch on the turn indicator
+                  expression: "'turn indicator' IS 'on' OR 'direction' IS 'straight'"
+            - constraints may also be part of a structure. Then, they define the relation between the subparameters of the structure
+
+            - an example of a constraint in a structure:
+              parameter: person
+              parameters:
+                - parameter: name
+                    choices: [John, Jane]
+                - parameter: age
+                    choices: [young, old]
+                - parameter : occupation
+                    choices: [student, worker]
+              logic:
+              - constraint: John is a student
+                expression: "IF 'name' IS 'John' THEN 'occupation' IS 'student'"
+              - constraint: youn people are students
+                expression: "IF 'age' IS 'young' THEN 'occupation' IS 'student'"
+
+            - if a constraint is defined in a global parameter, it is used in all parameters linked to this global parameter
+            - a constraint in a global parameter can be optionally disabled in a linking parameter, using an optional element 'constraints whitelist' or 'constraints blacklist'
+            - 'constraints whitelist' is a list of names of constraints that will be used in the linked parameter
+            - 'constraints blacklist' is a list of names of constraints that will not be used in the linked parameter
+                  
+            - assignment defines a value of an output parameter that is calculated from other parameters
+            - assignment has a form: IF EXPR THEN ASSIGNMENT_1, ASSIGNMENT_2, ...
+            - ASSIGNMENT_1, ASSIGNMENT_2, ... are assignments of the output parameter. They have a form: 'output parameter' = 'value'
+            - 'value' is a string that will be assigned to the output parameter. It is not an expression to be evaluated during generation. It does not need to be a name of any element in the model
+            - an example of an assignment to an output parameter:
+                parameters:
+                - parameter: sex
+                  choices: [M, F]
+                - output parameter: average height
+                  default value: 180
+
+                logic:
+                - assignment: woman is shorter
+                  expression: "IF 'sex' IS 'F' THEN 'average height' = '170'"
+            - When creatiung an expression, ensure that all logical expressions and constraints correctly represent relationships between parameters and that they adhere to the established syntax and structure guidelines. Double-check the use of logical operators, parameter names, and ensure that constraints are appropriately defined and placed.
             """},
         {
             "type": "text", 
-            "text": f"""These are example models:
-Model 1:            
+            "text": f"""
+These are example models:
+
+This is a simplest model:
+functions:
+- function: duel
+  parameters:
+  - parameter: good guy
+    choices: [Peter, Susan, Edmund, Lucy]
+  - parameter: bad guy
+    choices: [Jadis, Maugrim]
+  - parameter: location
+    choices: [White Castle, Cair Paravel]
+It has only one function with three parameters. The parameters are simple choices.
+
+            
+This is a model that uses global parameters:
+
 global parameters:
 - parameter: Weapon
   choices: [sword, bow, dagger]
@@ -78,8 +163,9 @@ functions:
     choices: [Jadis, Maugrim, a giant]
   - linked parameter: bad guy's weapon
     linked to: Weapon
-
-Model 2:
+Because both good guy and bad guy have a weapon, the weapon is a global parameter, so it does not need to be defined twice in the function. This facilitates maintenance of the model.
+    
+This model has a global parameter that is linked from another global parameter:
 global parameters:
 - parameter: Weapon
   choices: [sword, bow, dagger]
@@ -88,11 +174,7 @@ global parameters:
   - parameter: name
     choices: [Peter, Susan, Edmund, Lucy]
   - parameter: gender
-    choices: 
-    - choice: M
-      value: male
-    - choice: F
-      value: female
+    choices: [F, M]
   - linked parameter: weapon
     linked to: Weapon
 
@@ -104,12 +186,18 @@ functions:
   - linked parameter: contestant 2
     linked to: Character
 
-Model 3:
+This example contains  nested choices:
 functions:
 - function: character
   parameters:
   - parameter: name
     choices: [Peter, Susan, Lucy, Edmund]
+  - parameter: sex
+    choices: 
+    - choice: M
+      value: male
+    - choice: F
+      value: Female
   - parameter: location
     choices: [Cair Paravel, Stone Table, Lantern Waste]
   - parameter: weapon
@@ -120,6 +208,17 @@ functions:
       choices: [sword, dagger]
     - choice: charisma
     - choice: magic
+  logic:
+  - alias: girl
+    expression: "'sex' IS 'F'"
+  - constraint: girls name
+    expression: "IF 'girl' THEN 'name' IN ['Susan', 'Lucy']"
+  - constraint: boys name
+    expression: "IF NOT 'girl' THEN 'name' IN ['Peter', 'Edmund']"
+  - constraint: boys cant use ranged weapons
+    expression: "IF NOT 'girl' THEN 'weapon' IS NOT 'ranged'"
+Here, the 'weapon' parameter has a choice 'ranged' that has its own choices. This makes it easier to use in a constraint. 
+Using shorter name of the choice in the constraint makes the model more readable. The alias allows to easily reuse an expression in many constraints.
 """},
 
     ]},
